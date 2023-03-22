384205af059c591a8b5a1ccf3ac39cd0faf9e8d8
[oleksandr_smyrnov] <[oleksandrsmyrnov.work@gmail.com]git config --global user.email [oleksandrsmyrnov.work@gmail.com]>
2023-03-22 20:48:45 +0100

    Create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">e-Shop</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
